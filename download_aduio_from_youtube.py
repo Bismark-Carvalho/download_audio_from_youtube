79a4d5d427e8050f90e24fce160cf351fd6a48fe
Bismark de Carvalho <47608746+Bismark-Carvalho@users.noreply.github.com>
2022-01-07 22:18:07 -0300

    remoção da url

--- download_aduio_from_youtube.py
+++ download_aduio_from_youtube.py
@@ -4,7 +4,7 @@ import youtube_dl
 def run():
 
     video_info = youtube_dl.YoutubeDL().extract_info(
-        url="https://www.youtube.com/watch?v=9Ug8OdslZg4&list=RD9Ug8OdslZg4&start_radio=1&ab_channel=DeccaClassics",
+        url="SUA URL AQUI",
         download=False,
     )
     filename = f"{video_info['title']}.mp3"
